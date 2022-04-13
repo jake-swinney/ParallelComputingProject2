ad33fc5a96731040a6b4a3bb3d00b8da42ff47bf
ryanmobley <ryanmobley024@gmail.com>
2022-04-13 03:14:02 -0400

    this p2p works i suppose

--- TCPClient.java
+++ TCPClient.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.StandardOpenOption;
 
 public class TCPClient
 {
@@ -20,21 +19,22 @@ public class TCPClient
             System.exit(1);
         }
 
+        String routerName = "127.0.0.1"; // ServerRouter host name
+        int routerPort = 5555; // port number
+
         // Variables for setting up connection and communication
-        Socket Socket = null; // socket to connect with ServerRouter
+        Socket socket = null; // socket to connect with ServerRouter
         PrintWriter out = null; // for writing to ServerRouter
         BufferedReader in = null; // for reading form ServerRouter
-        InetAddress addr = InetAddress.getLocalHost();
-        String host = addr.getHostAddress(); // Client machine's IP
-        String routerName = "192.168.0.100"; // ServerRouter host name - Raspberry Pi
-        int SockNum = 5555; // port number
+        //InetAddress addr = InetAddress.getLocalHost();
+        //String host = addr.getHostAddress(); // Client machine's IP
 
         // Tries to connect to the ServerRouter
         try
         {
-            Socket = new Socket(routerName, SockNum);
-            out = new PrintWriter(Socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
+            socket = new Socket(routerName, routerPort);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         }
         catch (UnknownHostException e)
         {
@@ -46,31 +46,58 @@ public class TCPClient
             System.err.println("Couldn't get I/O for the connection to: " + routerName);
             System.exit(1);
         }
+        
+        // Once a connection is established to the ServerRouter, state whether this is a Sender or Receiver
+        System.out.println("Message to ServerRouter: Sender");
+        out.println("Sender");
+        
+        // Response is the IP and port of the receiver client that is found
+        String response = in.readLine();
+        System.out.println("Response from ServerRouter: " + response);
+        
+        if (response.equals("NO_RECEIVER_AVAILABLE"))
+        {
+        	System.out.println("Client could not connect; no receiver was available. Exiting.");
+        	System.exit(1);
+        }
+        
+        String[] parts = response.split(":");
+        
+        String receiverAddress = parts[0];
+        int receiverPort = Integer.parseInt(parts[1]);
+        
+        // Communication with ServerRouter finished
+        in.close();
+        out.close();
+        socket.close();
 
+        // Make connection to Receiver client
+        // TODO: Try/catch
+        socket = new Socket(receiverAddress, receiverPort);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
         // Variables for message passing
         String fromServer; // messages received from ServerRouter
         String fromUser; // messages sent to ServerRouter
-        String address = "192.168.0.101"; // destination IP (Server) - laptop
+        //String address = "192.168.0.101"; // destination IP (Server) - laptop
 
+        
         // Communication process (initial sends/receives)
-        //out.println(address); // initial send (IP of the destination Server)
-        out.println("5554");
-        String rcvdAddr = in.readLine();
-        int rcvdPort = Integer.parseInt(in.readLine());
-        Socket.close();
-
-        Socket outSocket = new Socket(rcvdAddr, rcvdPort);
-        out = new PrintWriter(outSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(outSocket.getInputStream()));
+        /*out.println(address); // initial send (IP of the destination Server)
+        fromServer = in.readLine(); // initial receive from router (verification of connection)
+        System.out.println("ServerRouter: " + fromServer);
 
         String rtTime = "";
         fromServer = in.readLine(); // receive RTTime
+        System.out.println("ServerRouter: " + fromServer);
         if (fromServer.startsWith("!RTTime"))
         {
             rtTime = fromServer.substring(9);
         }
 
         out.println(host); // Client sends the IP of its machine as initial send
+		*/
 
         if (fileName.endsWith(".txt"))
         {
@@ -86,6 +113,11 @@ public class TCPClient
             int charCount = 0;
             int numMsg = 0;
             long startTime = System.currentTimeMillis();
+            
+            // ADDED - client now sends first
+            fromUser = fromFile.readLine();
+            System.out.println("Client: " + fromUser);
+            out.println(fromUser);
 
             // Communication while loop
             while ((fromServer = in.readLine()) != null)
@@ -94,10 +126,10 @@ public class TCPClient
                 t1 = System.currentTimeMillis();
                 if (fromServer.equals("Bye.")) // exit statement
                     break;
-                else if(fromServer.startsWith("!RTTime"))
+                /*else if(fromServer.startsWith("!RTTime"))
                 {
                     rtTime = fromServer.substring(9);
-                }
+                }*/
 
                 t = t1 - t0;
                 System.out.println("Cycle time: " + t);
@@ -127,21 +159,18 @@ public class TCPClient
             long avgMsgTime = totalTime / numMsg;
 
             //Write stuff to CSV file
-            File csv = new File("data.csv");
+            /*File csv = new File("data.csv");
             csv.createNewFile();
 
             BufferedWriter outCsv = new BufferedWriter(new FileWriter(csv, true));
             outCsv.write(fileName + "," + avgMsgSize + "," + avgMsgTime + "," + rtTime + "," + fileSize + "\n");
-            outCsv.close();
+            outCsv.close();*/
         }
         else
         {
             // Receive first message (server's host) from server
-            while ((fromServer = in.readLine()) != null)
-            {
+            if ((fromServer = in.readLine()) != null)
                 System.out.println("Server: " + fromServer);
-                break;
-            }
 
             // Send the server the name of the file
             fromUser = "!FILENAME:" + fileName;
@@ -158,12 +187,15 @@ public class TCPClient
             fromUser = "!BYTES:" + data.length;
             System.out.println(fromUser);
             out.println(fromUser);
-            OutputStream os = Socket.getOutputStream();
+
+            // Set start time and begin sending the file
+            long t0 = System.currentTimeMillis();
+
+            OutputStream os = socket.getOutputStream();
             os.write(data, 0, data.length);
             os.flush();
 
             System.out.println("Sent " + data.length + " bytes.");
-            long t0 = System.currentTimeMillis();
 
             while ((fromServer = in.readLine()) != null)
             {
@@ -178,20 +210,18 @@ public class TCPClient
             System.out.println("RTT: " + t);
 
             //Write stuff to CSV file
-            File csv = new File("data.csv");
+            /*File csv = new File("data.csv");
             csv.createNewFile();
 
             BufferedWriter outCsv = new BufferedWriter(new FileWriter(csv, true));
             outCsv.write(fileName + "," + data.length + "," + t + "," + rtTime + "\n");
-            outCsv.close();
+            outCsv.close();*/
         }
 
         System.out.println("Closing connection.");
-
         // closing connections
-        outSocket.close();
         out.close();
         in.close();
-        Socket.close();
+        socket.close();
     }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -5,21 +5,22 @@ public class TCPServer
 {
     public static void main(String[] args) throws IOException
     {
+        String routerName = "127.0.0.1"; // ServerRouter host name
+        int routerPort = 5556; // port number
+        
         // Variables for setting up connection and communication
-        Socket Socket = null; // socket to connect with ServerRouter
+        Socket socket = null; // socket to connect with ServerRouter
         PrintWriter out = null; // for writing to ServerRouter
         BufferedReader in = null; // for reading form ServerRouter
-        InetAddress addr = InetAddress.getLocalHost();
-        String host = addr.getHostAddress(); // Server machine's IP
-        String routerName = "192.168.0.100"; // ServerRouter host name - Raspberry Pi
-        int SockNum = 5556; // port number
+        //InetAddress addr = InetAddress.getLocalHost();
+        //String host = addr.getHostAddress(); // Server machine's IP
 
         // Tries to connect to the ServerRouter
         try
         {
-            Socket = new Socket(routerName, SockNum);
-            out = new PrintWriter(Socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
+            socket = new Socket(routerName, routerPort);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         }
         catch (UnknownHostException e)
         {
@@ -31,27 +32,53 @@ public class TCPServer
             System.err.println("Couldn't get I/O for the connection to: " + routerName);
             System.exit(1);
         }
+        
+        // Once a connection is established to the ServerRouter, state whether this is a Sender or Receiver
+        System.out.println("Message to ServerRouter: Receiver");
+        out.println("Receiver");
+        
+        // Response is the port number that the server will listen on
+        String response = in.readLine();
+        System.out.println("Response from ServerRouter: " + response);
+        
+        //while (response == null)
+        //	response = in.readLine();
+        
+        if (response.equals("ROUTING_TABLE_FULL"))
+        {
+        	System.out.println("Sender client could not connect; routing table was full. Exiting.");
+        	System.exit(1);
+        }
+        
+        int receiverPort = Integer.parseInt(response);
+        
+        // Communication with ServerRouter finished
+        in.close();
+        out.close();
+        socket.close();
+        
+        // Start a ServerSocket to listen for a client. Close after opening a socket to the first client.
+        System.out.println("Opening a ServerSocket on port " + receiverPort);
+        ServerSocket serverSocket = new ServerSocket(receiverPort);
+        socket = serverSocket.accept();
+        serverSocket.close();
+        
+        // Make new reader/writer for message passing with sender client
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         // Variables for message passing
         String fromServer; // messages sent to ServerRouter
         String fromClient; // messages received from ServerRouter
-        String address = "192.168.0.103"; // destination IP (Client) - desktop
+        //String address = "192.168.0.103"; // destination IP (Client) - desktop
 
         // Communication process (initial sends/receives)
-        out.println(5557);
-        Socket.close();
-
-        ServerSocket inSocket = new ServerSocket(5557);
-        Socket = inSocket.accept();
-        in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
-        out = new PrintWriter(Socket.getOutputStream(), true);
-
-        out.println(address); // initial send (IP of the destination Client)
+        /*out.println(address); // initial send (IP of the destination Client)
         fromClient = in.readLine(); // initial receive from router (verification of connection)
         System.out.println("ServerRouter: " + fromClient);
 
         fromServer = in.readLine(); // receive RTTime
-        System.out.println("ServerRouter: " + fromServer);
+        System.out.println("ServerRouter: " + fromServer);*/
 
         String fileName = null;
 
@@ -85,7 +112,7 @@ public class TCPServer
 
                 System.out.println("Receiving " + numBytes + " bytes.");
 
-                InputStream inStream = Socket.getInputStream();
+                InputStream inStream = socket.getInputStream();
 
                 bytesRead = inStream.read(data, 0, numBytes);
                 current = bytesRead;
@@ -121,9 +148,8 @@ public class TCPServer
 
         System.out.println("Closing connection.");
         // closing connections
-        inSocket.close();
         out.close();
         in.close();
-        Socket.close();
+        socket.close();
     }
 }
